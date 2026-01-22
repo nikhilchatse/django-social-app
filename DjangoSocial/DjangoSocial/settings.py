135d8893e2c545445416778e03c2840041f55179
Nikhil-Chatse <nikhilchatse143@gmail.com>
2026-01-22 22:54:39 +0530

    root fixedd

--- DjangoSocial/DjangoSocial/settings.py
+++ DjangoSocial/DjangoSocial/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["django-social-app-nikhil.onrender.com",
+    "localhost",
+    "127.0.0.1"
+    ]
 
 
 # Application definition
--- DjangoSocial/profile_/urls.py
+++ DjangoSocial/profile_/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from .views import *
 urlpatterns = [
-    path('',home,name="home"),
+    path('/',home,name="home"),
     path('profile/',user_profile, name="profile"),
     path('edit/<int:id>',edit_profile, name="edit"),
     path('delete/<int:id>',delete_post, name="delete"),
