ac36703bf4fab0ff4d2a3843e5fa3c60c2d5b1d8
Nikhil-Chatse <nikhilchatse143@gmail.com>
2026-01-23 00:09:42 +0530

    migrations2

--- DjangoSocial/profile_/models.py
+++ DjangoSocial/profile_/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True)
     profile_img=models.ImageField(upload_to='profile_img',null=True,blank=True)
-    name=models.CharField(max_length=50)
-    contact=models.CharField(max_length=15)
-    email=models.CharField(max_length=50)
-    bio=models.TextField()
+    name=models.CharField(max_length=50,null=True,blank=True)
+    contact=models.CharField(max_length=15,null=True,blank=True)
+    email=models.CharField(max_length=50,null=True,blank=True)
+    bio=models.TextField(max_length=200,null=True,blank=True)
 
