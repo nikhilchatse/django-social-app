7e5f9ff9d295610f743f3861cb59ce935c043686
Nikhil-Chatse <nikhilchatse143@gmail.com>
2026-01-27 22:18:55 +0530

    debug media navbar

--- DjangoSocial/DjangoSocial/settings.py
+++ DjangoSocial/DjangoSocial/settings.py
@@ -137,3 +137,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
