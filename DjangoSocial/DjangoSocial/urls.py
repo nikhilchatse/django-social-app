5a64290c8960a26b81d8855bad275a2219690497
Nikhil-Chatse <nikhilchatse143@gmail.com>
2026-01-28 01:04:08 +0530

    done

--- DjangoSocial/DjangoSocial/urls.py
+++ DjangoSocial/DjangoSocial/urls.py
@@ -28,9 +28,9 @@ urlpatterns = [
     path('',include("posts.urls")),
 ] 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [
     re_path(r'^media/(?P<path>.*)$', serve, {
