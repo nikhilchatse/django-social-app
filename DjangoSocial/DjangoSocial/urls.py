facb5f208dda0ef935317266364743a13d01e2f4
Nikhil-Chatse <nikhilchatse143@gmail.com>
2026-01-27 18:31:21 +0530

    debug media

--- DjangoSocial/DjangoSocial/urls.py
+++ DjangoSocial/DjangoSocial/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve 
+from django.urls import re_path
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("profile_.urls")),
@@ -25,7 +28,12 @@ urlpatterns = [
     path('',include("posts.urls")),
 ] 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
